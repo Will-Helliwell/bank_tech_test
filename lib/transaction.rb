30d84cd2b6e95200836325e71af51faeb3880415
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-08 15:43:43 +0000

    transaction class initialized with current date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,6 +1,9 @@
 class Transaction
 
+  attr_reader :date
+
   def initialize(credit, debit, balance)
+    @date = Time.new.strftime("%d/%m/%Y")
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,11 +2,14 @@ require 'transaction'
 
 describe Transaction do
 
+  let(:transaction) {Transaction.new(10, 20, 30)}
+
   describe "initialization" do
     it "takes 3 arguments" do
       expect(described_class).to respond_to(:new).with(3).arguments
     end
-    xit "is created with a date attribute of the current date" do
+    it "is created with a date attribute of the current date" do
+      expect(transaction.date).to eq(Time.new.strftime("%d/%m/%Y"))
     end
   end
 
