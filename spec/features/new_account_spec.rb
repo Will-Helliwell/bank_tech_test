26ca47e0745d196d1bebfccb6ab86009e21738fc
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-09 12:18:34 +0000

    remove redundant feature test for new account

--- spec/features/new_account_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'account'
-
-describe "FEATURE TEST - create new account" do
-  let(:account) {Account.new}
-
-  it "a new user starts with a fresh account" do
-    expect(account.print_balance).to eq("Your current balance is 0")
-  end
-end
