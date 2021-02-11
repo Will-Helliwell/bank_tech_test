f67093c814ac0ceb6dcedd49eb3c0446bf252010
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-11 12:38:11 +0000

    add proof of concept for stubbing Time.new in feature test (commented out). Use to remove the optional date parameter that currently has to be passed into withdraw and dposit methods for test purposes

--- spec/features/print_statement_spec.rb
+++ spec/features/print_statement_spec.rb
@@ -18,11 +18,16 @@ describe 'FEATURE TESTS - print_statement' do
     account.deposit(amount: 100, date: '10/01/2012')
     account.deposit(amount: 2000, date: '13/01/2012')
     account.withdraw(amount: 500, date: '14/01/2012')
+
+    # Proof of concept for stubbing Time.new in tests
+    # allow(Time).to receive(:new).and_return(Time.mktime(1970, 1, 1))
+    # account.withdraw(amount: 500, date: Time.new.strftime(Account::DATE_FORMAT))
+
     expect(account.print_statement).to eq([
       Account::STATEMENT_HEADINGS,
       ['10/01/2012', 100, nil, Account::INITIAL_BALANCE + 100],
       ['13/01/2012', 2000, nil, Account::INITIAL_BALANCE + 100 + 2000],
       ['14/01/2012', nil, 500, Account::INITIAL_BALANCE + 100 + 2000 - 500]
-    ])                                    
+    ])
   end
 end
